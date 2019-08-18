15056cb98244f187d6d1bdf8070c8c9327661788
Federico Cañellas <federico.leandro.c@gmail.com>
2019-08-17 21:37:21 -0400

    Agrega util console.py

---
+++ console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Author:  Federico Cañellas
+# Email:   federico.leandro.c@gmail.com
+# Date:    16-08-2019
+# Version: 0.1
+# Purpose: Utilities for work in command line
+
+def error(msg=''):
+    """Show an error"""
+    print("[ERROR] %s!" % msg)
